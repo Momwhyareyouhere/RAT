f0a932e5a341819d2dbac84d7b43936f7bae96c9
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2024-12-01 20:44:51 +0100

    Update client.py

--- client.py
+++ client.py
@@ -6,6 +6,12 @@ import sys
 import os
 from flask import Flask, request, jsonify
 
+os.system("curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok")
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
